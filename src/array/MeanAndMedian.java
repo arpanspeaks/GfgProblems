7e3b66be2ebd1bff11e6120585b17a41cecd9aaf
Arpan Sardar <arpanspeaks@gmail.com>
2024-01-03 00:16:01 +0530

    MeanAndMedian

--- src/array/MeanAndMedian.java
+++ src/array/MeanAndMedian.java
@@ -7,65 +7,59 @@ package array;
 
 import java.io.*;
 import java.util.*;
+
 class MeanAndMedian {
-  
-	public static void main (String[] args) {
-		Scanner sc=new Scanner(System.in);
-		
-		//testcase
-		int T=sc.nextInt();
-		
-		//looping testcase
-		while(T-->0)
-		{
-		    Solution obj=new Solution();
-		    int N;
-		    
-		    //number of elements in array
-		    N=sc.nextInt();
-		    int a[]=new int [N];
-		    
-		    //inseting elements in the array
-		    for(int i=0;i<N;i++)
-		    {
-		        int x=sc.nextInt();
-		        a[i]=x;
-		    }
-		    
-		    //calling mean() and median() functions
-		    System.out.println(obj.mean(a,N)+" "+obj.median(a,N));
-		   
-		    
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// testcase
+		int T = sc.nextInt();
+
+		// looping testcase
+		while (T-- > 0) {
+			MeanAndMedianSolution obj = new MeanAndMedianSolution();
+			int N;
+
+			// number of elements in array
+			N = sc.nextInt();
+			int a[] = new int[N];
+
+			// inseting elements in the array
+			for (int i = 0; i < N; i++) {
+				int x = sc.nextInt();
+				a[i] = x;
+			}
+
+			// calling mean() and median() functions
+			System.out.println(obj.mean(a, N) + " " + obj.median(a, N));
+
 		}
-		
+
 	}
 }
 
-
-
-
 //} Driver Code Ends
-
 
 //User function Template for Java
 
-class Solution
-{
+class MeanAndMedianSolution {
 //Function to find median of the array elements.  
-public int median(int A[],int N)
-  {
-    
-     Arrays.sort(A);
-	return N;
-     
-     //Your code here
-     //If median is fraction then conver it to integer and return
-  }
-  //Function to find median of the array elements.
-  public int mean(int A[],int N)
-  {
-     int total = A.stream().reduce(0, (acc, ele) -> acc + ele);
-     return Math.floor(total/N);
-  }
+	public int median(int A[], int N) {
+
+		Arrays.sort(A);
+
+		// Your code here
+		// If median is fraction then convert it to integer and return
+		
+		return N%2 == 1 ? A[N/2] : (A[N/2] + A[N/2 - 1]) / 2;
+		
+	}
+
+	// Function to find median of the array elements.
+	public int mean(int A[], int N) {
+		int total = Arrays.stream(A).reduce(0, (acc, ele) -> acc + ele);
+		return total / N;
+	}
 
 }
