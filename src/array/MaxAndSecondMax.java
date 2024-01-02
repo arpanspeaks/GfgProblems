2488d66496d7f4be37644bfea9968d9106215330
Coding Sardar <sardarcoding@gmail.com>
2024-01-02 23:47:04 +0530

    MaxAndSecondMax

---
+++ src/array/MaxAndSecondMax.java
@@ -0,0 +1,65 @@
+package array;
+
+// Link: https://www.geeksforgeeks.org/batch/placement-100-2019/track/DSASP-Arrays/problem/max-and-second-max
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+//} Driver Code Ends
+
+//Solution class to implement function largestAndSecondLargest
+class Solution {
+
+	// Function to find largest and second largest element in the array
+	public static ArrayList<Integer> largestAndSecondLargest(int sizeOfArray, int arr[]) {
+		int largest = -1, secondLargest = -1;
+		for(int ele : arr) {
+			if(ele > largest) {
+				secondLargest = largest;
+				largest = ele;
+			} else if(ele > secondLargest && ele != largest) {
+				secondLargest = ele;
+			}
+		}
+		return new ArrayList<>(Arrays.asList(largest, secondLargest));
+	}
+}
+
+//{ Driver Code Starts.
+
+//Driver class
+class MaxAndSecondMax {
+
+	// Driver code
+	public static void main(String[] args) throws IOException {
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int testcases = Integer.parseInt(br.readLine());
+
+		// looping through all testcases
+		while (testcases-- > 0) {
+			int sizeOfArray = Integer.parseInt(br.readLine());
+
+			int arr[] = new int[sizeOfArray];
+
+			String line = br.readLine();
+			String[] elements = line.trim().split("\\s+");
+
+			for (int index = 0; index < sizeOfArray; index++) {
+				arr[index] = Integer.parseInt(elements[index]);
+			}
+
+			Solution obj = new Solution();
+			ArrayList<Integer> res = new ArrayList<Integer>();
+			res = obj.largestAndSecondLargest(sizeOfArray, arr);
+			System.out.println(res.get(0) + " " + res.get(1));
+		}
+	}
+}
+//} Driver Code Ends
